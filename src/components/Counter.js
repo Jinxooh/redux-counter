36aae24f1d6b72cb8541c608aeb1fd78378b562e
Jeckson <cjswp122@gmail.com>
2017-05-18 21:07:43 +0900

    third commit : add redux and CounterList

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Counter.css';
 
-const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
     return (
         <div 
             className="Counter" 
-            onClick={onIncrement} 
+            onClick={() => onIncrement(index)} 
             onContextMenu={
                 (e) => { 
                     e.preventDefault(); 
-                    onDecrement();
+                    onDecrement(index);
                 }
             } 
-            onDoubleClick={onSetColor}
+            onDoubleClick={ () => onSetColor(index)}
             style={{backgroundColor: color}}>
                 {number}
         </div>
@@ -23,6 +23,7 @@ const Counter = ({number, color, onIncrement, onDecrement, onSetColor}) => {
 Counter.propTypes = {
     number: PropTypes.number,
     color: PropTypes.string,
+    index: PropTypes.number,
     onIncrement: PropTypes.func,
     onDecrement: PropTypes.func,
     onSetColor: PropTypes.func
@@ -31,6 +32,7 @@ Counter.propTypes = {
 Counter.defaultProps = {
     number: 0,
     color: 'black',
+    index: 0,
     onIncrement: () => console.warn('onIncrement not defined'),
     onDecrement: () => console.warn('onDecrement not defined'),
     onSetColor: () => console.warn('onSetColor not defined')
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,26 +1,74 @@
-import number from './number';
-import color from './color';
+import * as types from '../actions/ActionTypes';
 
-import { combineReducers } from 'redux';
-
-/*
-    서브 리듀서들을 하나로 합칩니다.
-    combineReducers 를 실행하고 나면, 나중에 store의 형태가 파라미터로 전달한 객체의 모양대로 만들어집니다.
-    지금의 경우:
-    {
-        numberData: {
+const initialState = {
+    counters: [
+        {
+            color: 'black',
             number: 0
-        },
-        colorData: {
-            color: 'black'
         }
+    ]
+}
+
+const counter = (state = initialState, action) => {
+    const { counters } = state;
+
+    switch(action.type) {
+        case types.CREATE:
+            return {
+                counters: [
+                    ...counters,
+                    {
+                        color: action.color,
+                        number: 0
+                    }
+                ]
+            };
+        case types.REMOVE:
+            return {
+                counters: counters.slice(0, counters.length - 1)
+            };
+
+        // action.index 번째 카운터의 number 에 1 을 더합니다.
+        case types.INCREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number + 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
+
+        // action.index 번째 카운터의 number 에 1 을 뺍니다
+        case types.DECREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number - 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
+
+        // action.index 번째 카운터의 색상을 변경합니다
+        case types.SET_COLOR:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        color: action.color
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
+        default:
+            return state;
     }
-    로 만들어집니다.
-*/
+};
 
-const reducers = combineReducers({
-    numberData: number,
-    colorData: color
-});
-
-export default reducers;
+export default counter;
